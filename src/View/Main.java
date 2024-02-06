5eeeb8ed7a47b48edc31e4e825df4947742519d0
Robin Blondin <robin.blondin@protonmail.com>
2024-02-06 09:02:42 +0100

    test methods

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,13 +1,11 @@
 package View;
 
+import Controller.Reporter;
 import Controller.Repository;
 
 public class Main {
     public static void main(String[] args) {
-        Repository dh = new Repository();
-
-        System.out.println(dh.getOrders().size());
-        dh.addToCart(1, 16, 3);
-        System.out.println(dh.getOrders().size());
+       Reporter r = new Reporter();
+       r.customerPurchases("38", "black", "vagabond");
     }
 }
