5d05bdcfe6a75edf4bc4d2a9b1eb7c6d31f810f1
Robin Blondin <robin.blondin@protonmail.com>
2024-02-06 23:55:49 +0100

    Created singleton class with basic style settings

---
+++ src/View/StyleSettings.java
@@ -0,0 +1,88 @@
+package View;
+
+import java.awt.*;
+
+public class StyleSettings {
+    private static StyleSettings instance;
+
+    private final Font bigFont;
+    private final Font mediumFont;
+    private final Font smallFont;
+    private final Color backgroundColor_LIGHT;
+    private final Color backgroundColor_DARK;
+    private final Color backgroundColor_SELECTED;
+    private final Color textColor_BLACK;
+    private final Color textColor_WHITE;
+    private final Color textColor_SELECTED;
+    private final Color textColor_DARK;
+    private final Color textColor_LIGHT;
+    private final Color buttonColor;
+
+    public StyleSettings() {
+        bigFont = new Font("Sans Serif", Font.PLAIN, 35);
+        mediumFont = new Font("Sans Serif", Font.PLAIN, 25);
+        smallFont = new Font("Sans Serif", Font.PLAIN, 18);
+        backgroundColor_LIGHT = new Color(50, 49, 62);
+        backgroundColor_DARK = new Color(39, 39, 48);
+        backgroundColor_SELECTED = new Color(85, 84, 97);
+        textColor_BLACK = new Color(0, 0, 0);
+        textColor_WHITE = new Color(255, 255, 255);
+        textColor_SELECTED = new Color(157, 157, 164);
+        textColor_DARK = new Color(98, 98, 107);
+        textColor_LIGHT = new Color(157, 172, 174);
+        buttonColor = new Color(158, 119, 226);
+
+    }
+    public static StyleSettings getInstance() {
+        if (instance == null) {
+            instance = new StyleSettings();
+        }
+        return instance;
+    }
+
+    public Font getBigFont() {
+        return bigFont;
+    }
+
+    public Font getMediumFont() {
+        return mediumFont;
+    }
+
+    public Font getSmallFont() {
+        return smallFont;
+    }
+
+    public Color getBackgroundColor_LIGHT() {
+        return backgroundColor_LIGHT;
+    }
+
+    public Color getBackgroundColor_DARK() {
+        return backgroundColor_DARK;
+    }
+
+    public Color getBackgroundColor_SELECTED() {
+        return backgroundColor_SELECTED;
+    }
+
+    public Color getTextColor_BLACK() {
+        return textColor_BLACK;
+    }
+
+    public Color getTextColor_WHITE() {
+        return textColor_WHITE;
+    }
+
+    public Color getTextColor_SELECTED() {
+        return textColor_SELECTED;
+    }
+
+    public Color getTextColor_DARK() {
+        return textColor_DARK;
+    }
+
+    public Color getTextColor_LIGHT() {
+        return textColor_LIGHT;
+    }
+
+    public Color getButtonColor() {return buttonColor;}
+}
