ad9f49936793587d03560cbf4cf5dc2dca664b32
Robin Blondin <robin.blondin@protonmail.com>
2024-02-06 23:50:17 +0100

    Added actionListener class

---
+++ src/Controller/ActionListener.java
@@ -0,0 +1,17 @@
+package Controller;
+
+import View.MainFrame;
+
+import java.awt.event.ActionEvent;
+
+public class ActionListener implements java.awt.event.ActionListener {
+    private final MainFrame mainFrame;
+
+    public ActionListener(MainFrame mainFrame) {
+        this.mainFrame = mainFrame;
+    }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+
+    }
+}
