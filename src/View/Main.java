a1ba9f177ea22f18d2a587db4bb4cc5f4ca86ca4
Robin Blondin <robin.blondin@protonmail.com>
2024-02-06 17:08:17 +0100

    test methods

--- src/View/Main.java
+++ src/View/Main.java
@@ -6,6 +6,12 @@ import Controller.Repository;
 public class Main {
     public static void main(String[] args) {
        Reporter r = new Reporter();
-       r.customerPurchases("38", "black", "vagabond");
+       //r.customerPurchases("38", "black", "vagabond");
+       //r.ordersPerCustomer();
+       //r.moneySpentByCustomer();
+       //r.moneySpentPerCity();
+       //r.mostSoldProducts();
+        r.filterShoes("vagabond", "brown", "", "");
+
     }
 }
