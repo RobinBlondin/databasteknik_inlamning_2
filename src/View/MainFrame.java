180a2814ed3f457108e9d8570b76f5ef2cd32d6e
Robin Blondin <robin.blondin@protonmail.com>
2024-02-07 19:47:10 +0100

    Set up a cardLayout in mainFrame and moved UI code to own class

--- src/View/MainFrame.java
+++ src/View/MainFrame.java
@@ -9,56 +9,59 @@ import java.awt.*;
 public class MainFrame extends JFrame {
     private final Reporter reporter;
     private final Repository repo;
-    private final ListPanel listPanel;
+    private final ShopPanel shopPanel;
+    private final CardLayout cardLayout;
+    private final JPanel cards;
+    private final LoginPanel loginPanel;
 
     public MainFrame() {
+        StyleSettings style = StyleSettings.getInstance();
+
         reporter = new Reporter();
         repo = new Repository();
-        listPanel = new ListPanel(this, reporter, repo);
+        shopPanel = new ShopPanel(repo, reporter, this);
+        cards = new JPanel(new CardLayout());
+        loginPanel = new LoginPanel(this);
 
-        StyleSettings style = StyleSettings.getInstance();
-        this.setLayout(new BorderLayout());
-        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        this.setSize(new Dimension(1000, 1000));
         this.setTitle("Soles with soul");
+        this.setSize(1000, 1000);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 
-        JPanel topPanel = new JPanel();
-        topPanel.setLayout(new BorderLayout());
-        ImageIcon icon = new ImageIcon("images/logo.png");
-        icon = new ImageIcon(icon.getImage().getScaledInstance(67, 67, Image.SCALE_SMOOTH));
-        JLabel logoLabel = new JLabel(icon);
-        topPanel.setBackground(style.getBackgroundColor_SELECTED());
-        topPanel.setPreferredSize(new Dimension(1000, 75));
 
-        JPanel emptyPanel = new JPanel();
-        emptyPanel.setBackground(style.getBackgroundColor_DARK());
-        emptyPanel.setPreferredSize(new Dimension(200, 100));
-        emptyPanel.setVisible(true);
+        cards.setSize(new Dimension(1000, 1000));
+        cards.add(shopPanel, "shop");
+        cards.add(loginPanel, "login");
 
-        JPanel sidePanel = new JPanel();
-        sidePanel.setLayout(new GridLayout(8, 1));
-        sidePanel.setBackground(style.getBackgroundColor_DARK());
-        sidePanel.setPreferredSize(new Dimension(200, 1000));
-        sidePanel.add(emptyPanel);
-        sidePanel.add(new ButtonPanel("Button1", this));
-        sidePanel.add(new ButtonPanel("Button2", this));
-        sidePanel.add(new ButtonPanel("Button3", this));
-        sidePanel.add(new ButtonPanel("Button4", this));
-        sidePanel.add(new ButtonPanel("Button5", this));
-        sidePanel.add(new ButtonPanel("Button6", this));
+        cardLayout = (CardLayout) cards.getLayout();
 
-        JPanel centerPanel = new JPanel();
-        centerPanel.setLayout(new BorderLayout());
-        centerPanel.setBackground(style.getBackgroundColor_LIGHT());
-        centerPanel.setPreferredSize(new Dimension(800, 925));
-        centerPanel.add(new FilterPanel<>(repo), BorderLayout.NORTH);
-        centerPanel.add(listPanel, BorderLayout.CENTER);
 
-        this.add(topPanel, BorderLayout.NORTH);
-        this.add(sidePanel, BorderLayout.WEST);
-        this.add(centerPanel, BorderLayout.CENTER);
+
+        this.getContentPane().add(cards);
+        cardLayout.show(cards, "shop");
         this.setVisible(true);
+    }
 
+    public CardLayout getCardLayout() {
+        return cardLayout;
+    }
 
+    public ShopPanel getShopPanel() {
+        return shopPanel;
+    }
+
+    public LoginPanel getLoginPanel() {
+        return loginPanel;
+    }
+
+    public JPanel getCards() {
+        return cards;
+    }
+
+    public Reporter getReporter() {
+        return reporter;
+    }
+
+    public Repository getRepo() {
+        return repo;
     }
 }
