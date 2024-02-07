9330334d73d610b7415a19904dc4f33362bb59c4
Robin Blondin <robin.blondin@protonmail.com>
2024-02-07 19:45:06 +0100

    test code

--- src/Controller/ActionListener.java
+++ src/Controller/ActionListener.java
@@ -1,17 +1,31 @@
 package Controller;
 
+import View.LoginPanel;
 import View.MainFrame;
+import View.ShopPanel;
 
+import javax.swing.*;
 import java.awt.event.ActionEvent;
 
 public class ActionListener implements java.awt.event.ActionListener {
     private final MainFrame mainFrame;
+    private final ShopPanel shopPanel;
+    private final LoginPanel loginPanel;
 
-    public ActionListener(MainFrame mainFrame) {
+    public ActionListener(MainFrame mainFrame, ShopPanel shopPanel, LoginPanel loginPanel) {
         this.mainFrame = mainFrame;
+        this.shopPanel = shopPanel;
+        this.loginPanel = loginPanel;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
+
+        JButton pressed = (JButton)e.getSource();
+
+        if (pressed.getText().contains("1")) {
+            System.out.println("button 1 pressed");
+            mainFrame.getCardLayout().show(mainFrame.getCards(), "login");
+        }
 
     }
 }
