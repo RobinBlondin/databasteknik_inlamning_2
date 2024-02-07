8a136cfed342eba34cacf22844603e4928a26681
Robin Blondin <robin.blondin@protonmail.com>
2024-02-07 19:48:23 +0100

    cleaned up code

--- src/View/Main.java
+++ src/View/Main.java
@@ -1,15 +1,10 @@
 package View;
 
 import Controller.Reporter;
-import Controller.Repository;
-
-import java.util.ArrayList;
-import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         Reporter r = new Reporter();
         new MainFrame();
-
     }
 }
