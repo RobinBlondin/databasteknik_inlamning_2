4b952f9405db12e2dd30711dee6ba8819155bb34
Robin Blondin <robin.blondin@protonmail.com>
2024-02-08 21:34:01 +0100

    Changed buttons to custom buttons and tweaked the design

--- src/View/LoginPanel.java
+++ src/View/LoginPanel.java
@@ -6,107 +6,68 @@ import javax.swing.*;
 import java.awt.*;
 
 public class LoginPanel extends JPanel {
+    private final ButtonPanel loginButton;
+    private final ButtonPanel exitButton;
+    private JTextField userField;
+    private JTextField passField;
+    private JLabel errorLabel;
     public LoginPanel(MainFrame mainFrame) {
         StyleSettings style = StyleSettings.getInstance();
-        this.setBackground(style.getBackgroundColor_LIGHT());
+        this.setBackground(style.getBackgroundColor_DARK());
         this.setLayout(new BorderLayout());
         this.setPreferredSize(new Dimension(1000, 1000));
 
         JPanel westPanel = new JPanel();
         westPanel.setPreferredSize(new Dimension(300, 1000));
-        westPanel.setBackground(style.getBackgroundColor_LIGHT());
+        westPanel.setBackground(style.getBackgroundColor_DARK());
 
         JPanel eastPanel = new JPanel();
         eastPanel.setPreferredSize(new Dimension(300, 1000));
-        eastPanel.setBackground(style.getBackgroundColor_LIGHT());
+        eastPanel.setBackground(style.getBackgroundColor_DARK());
 
         JPanel southPanel = new JPanel();
         southPanel.setPreferredSize(new Dimension(1000, 400));
-        southPanel.setBackground(style.getBackgroundColor_LIGHT());
+        southPanel.setBackground(style.getBackgroundColor_DARK());
 
         JPanel northPanel = new JPanel();
         northPanel.setPreferredSize(new Dimension(1000, 400));
-        northPanel.setBackground(style.getBackgroundColor_LIGHT());
+        northPanel.setBackground(style.getBackgroundColor_DARK());
 
         JPanel centerPanel = new JPanel();
-        centerPanel.setBackground(style.getBackgroundColor_LIGHT());
-        centerPanel.setPreferredSize(new Dimension(400, 400));
+        centerPanel.setBackground(style.getBackgroundColor_DARK());
+        centerPanel.setPreferredSize(new Dimension(450, 400));
         centerPanel.setLayout(new BoxLayout(centerPanel, BoxLayout.Y_AXIS));
 
         JPanel buttonPanel = new JPanel();
         buttonPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
-        buttonPanel.setPreferredSize(new Dimension(400, 100));
-        buttonPanel.setBackground(style.getBackgroundColor_LIGHT());
+        buttonPanel.setPreferredSize(new Dimension(450, 100));
+        buttonPanel.setBackground(style.getBackgroundColor_DARK());
         buttonPanel.setBorder(BorderFactory.createEmptyBorder(0,0,20,0));
 
         JLabel emptyLabel = new JLabel();
         emptyLabel.setPreferredSize(new Dimension(50, 50));
 
-        JLabel errorLabel = new JLabel();
+        errorLabel = new JLabel();
         errorLabel.setPreferredSize(new Dimension(400, 40));
         errorLabel.setBorder(BorderFactory.createEmptyBorder(0, 40, 0, 50));
         errorLabel.setFont(style.getMicroFont());
         errorLabel.setForeground(Color.red);
 
-        JTextField userField = new JTextField();
-        JTextField passField = new JPasswordField();
+        userField = new JTextField();
+        passField = new JPasswordField();
 
         userField.setPreferredSize(new Dimension(200, 60));
         userField.setBorder(BorderFactory.createEmptyBorder(5, 10, 5,10));
-        userField.setFont(style.getMicroFont());
+        userField.setFont(style.getMicroFontBold());
+        userField.setBackground(style.getButtonColor());
 
         passField.setPreferredSize(new Dimension(200, 60));
         passField.setBorder(BorderFactory.createEmptyBorder(5, 10, 5,10));
-        passField.setFont(style.getMicroFont());
+        passField.setFont(style.getMicroFontBold());
+        passField.setBackground(style.getButtonColor());
 
-        JButton loginButton = new JButton("Login");
-        loginButton.setPreferredSize(new Dimension(100, 50));
-        loginButton.setBackground(style.getBackgroundColor_LIGHT());
-        loginButton.setForeground(style.getTextColor_LIGHT());
-        loginButton.setFont(style.getSmallFont());
-        loginButton.setBorder(BorderFactory.createEmptyBorder());
-        loginButton.setFocusPainted(false);
-        loginButton.addActionListener(a -> {
-            boolean isValidUser = mainFrame.getRepo()
-                    .getCustomers()
-                    .stream()
-                    .filter(user -> user.getEmail().equals(userField.getText()))
-                    .anyMatch(pass -> pass.getPassword().equals(passField.getText()));
-
-            if(isValidUser) {
-                mainFrame.getCardLayout().show(mainFrame.getCards(), "shop");
-
-                int currentId = mainFrame.getRepo()
-                        .getCustomers()
-                        .stream()
-                        .filter(user -> user.getEmail().equals(userField.getText()))
-                        .map(Customer::getId)
-                        .toList()
-                        .getFirst();
-
-                OrderEntry lastOrder = mainFrame.getRepo()
-                        .getOrders()
-                        .stream()
-                        .filter(order -> order.getCustomer().getId() == currentId)
-                        .toList()
-                        .getLast();
-
-                System.out.println(currentId);
-                mainFrame.getRepo().setLoggedInUserId(currentId);
-                mainFrame.getRepo().setLoggedInUsersLastOrder(lastOrder);
-            } else {
-                errorLabel.setText("Username or password is incorrect");
-            }
-        });
-
-        JButton exitButton = new JButton("Quit");
-        exitButton.setPreferredSize(new Dimension(100, 50));
-        exitButton.setBackground(style.getBackgroundColor_LIGHT());
-        exitButton.setForeground(style.getTextColor_LIGHT());
-        exitButton.setFont(style.getSmallFont());
-        exitButton.setBorder(BorderFactory.createEmptyBorder());
-        exitButton.setFocusPainted(false);
-        exitButton.addActionListener(a -> System.exit(0));
+        loginButton = new ButtonPanel(10, "Login", mainFrame, false);
+        exitButton = new ButtonPanel(3, "Quit", mainFrame, false);
 
         buttonPanel.add(loginButton);
         buttonPanel.add(emptyLabel);
@@ -127,5 +88,25 @@ public class LoginPanel extends JPanel {
         this.add(eastPanel, BorderLayout.EAST);
         this.add(centerPanel, BorderLayout.CENTER);
         this.setVisible(true);
+    }
+
+    public ButtonPanel getLoginButton() {
+        return loginButton;
+    }
+
+    public ButtonPanel getExitButton() {
+        return exitButton;
+    }
+
+    public JTextField getUserField() {
+        return userField;
+    }
+
+    public JTextField getPassField() {
+        return passField;
+    }
+
+    public JLabel getErrorLabel() {
+        return errorLabel;
     }
 }
