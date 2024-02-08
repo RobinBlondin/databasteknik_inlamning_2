7c051f9be894e9bd336a5006e2c9032a4f36fdf4
Robin Blondin <robin.blondin@protonmail.com>
2024-02-08 21:27:52 +0100

    Clean redundant code

--- src/Controller/ActionListener.java
+++ src/Controller/ActionListener.java
@@ -1,5 +1,6 @@
 package Controller;
 
+import View.ButtonPanel;
 import View.LoginPanel;
 import View.MainFrame;
 import View.ShopPanel;
@@ -9,23 +10,13 @@ import java.awt.event.ActionEvent;
 
 public class ActionListener implements java.awt.event.ActionListener {
     private final MainFrame mainFrame;
-    private final ShopPanel shopPanel;
-    private final LoginPanel loginPanel;
 
-    public ActionListener(MainFrame mainFrame, ShopPanel shopPanel, LoginPanel loginPanel) {
+    public ActionListener(MainFrame mainFrame) {
         this.mainFrame = mainFrame;
-        this.shopPanel = shopPanel;
-        this.loginPanel = loginPanel;
     }
     @Override
     public void actionPerformed(ActionEvent e) {
 
-        JButton pressed = (JButton)e.getSource();
-
-        if (pressed.getText().contains("1")) {
-            System.out.println("button 1 pressed");
-            mainFrame.getCardLayout().show(mainFrame.getCards(), "login");
-        }
 
     }
 }
