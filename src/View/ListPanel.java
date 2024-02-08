42a8e828582bcb6dbf453002cbc3e8d4d16d13fe
Robin Blondin <robin.blondin@protonmail.com>
2024-02-08 21:33:25 +0100

    Added several cases of ListLabel, depending on results of split

--- src/View/ListPanel.java
+++ src/View/ListPanel.java
@@ -26,7 +26,7 @@ public class ListPanel extends JPanel {
         this.setBackground(Color.WHITE);
         gridPanel = new JPanel();
         gridPanel.setLayout(new BoxLayout(gridPanel, BoxLayout.PAGE_AXIS));
-        gridPanel.setBorder(BorderFactory.createEmptyBorder());
+        gridPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, style.getButtonColor()));
         gridPanel.setBackground(style.getBackgroundColor_LIGHT());
         
         emptyPanel = new JPanel();
@@ -38,8 +38,7 @@ public class ListPanel extends JPanel {
         refresh(filteredList, 1);
 
         JScrollPane scrollPane = new JScrollPane(gridPanel,
-                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED,
-                JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+                JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED, JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
 
         scrollPane.setBackground(style.getBackgroundColor_LIGHT());
         add(scrollPane, BorderLayout.CENTER);
@@ -51,9 +50,10 @@ public class ListPanel extends JPanel {
         for (String str : list) {
             String[] arr = str.split(", ");
             switch(report) {
-                case 1 -> gridPanel.add(new ListLabel(Integer.parseInt(arr[0]), arr[1], arr[2], arr[3], String.valueOf(arr[4]), String.valueOf(arr[5]), repo, withCartButton));
-                case 2 -> gridPanel.add(new ListLabel(Integer.parseInt(arr[0]), arr[1], arr[3], arr[4], arr[5], "", repo, withCartButton));
-
+                case 1 -> gridPanel.add(new ListLabel(Integer.parseInt(arr[0]), repo, withCartButton, arr[1], arr[2], arr[3], arr[4], arr[5]));
+                case 2 -> gridPanel.add(new ListLabel(Integer.parseInt(arr[0]),repo, withCartButton, arr[1], arr[2], arr[3]));
+                case 3 -> gridPanel.add(new ListLabel(0, repo, withCartButton, arr[0], arr[1]));
+                case 4 -> gridPanel.add(new ListLabel(0, repo, withCartButton, arr[0], arr[1], arr[2]));
             }
         }
         for (int i = list.size(); i < MIN_ENTRIES; i++) {
@@ -70,7 +70,11 @@ public class ListPanel extends JPanel {
         this.repaint();
     }
 
-    public void removeFromUI(String platform, String userName) {
+    public void clearList() {
+        gridPanel.removeAll();
+    }
+
+    /*public void removeFromUI(String platform, String userName) {
         for (Component component : gridPanel.getComponents()) {
             if (component instanceof ListLabel listLabel) {
                 if (listLabel.getBrandText().equalsIgnoreCase(platform) && listLabel.getModelText().equalsIgnoreCase(userName)) {
@@ -83,7 +87,7 @@ public class ListPanel extends JPanel {
                 }
             }
         }
-    }
+    }*/
 
     /*public void filter(String brand, String model, String ) {
         gridPanel.removeAll();
@@ -99,5 +103,6 @@ public class ListPanel extends JPanel {
         gridPanel.revalidate();
         gridPanel.repaint();
     }*/
+
 }
 
