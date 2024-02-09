5e7fb8cf713cabb4011ba29286db8900beccdf78
Robin Blondin <robin.blondin@protonmail.com>
2024-02-09 01:48:48 +0100

    simplified lambda, variable rename

--- src/Controller/Reporter.java
+++ src/Controller/Reporter.java
@@ -6,24 +6,23 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class Reporter {
-    Repository rp;
+    Repository repo;
 
     public Reporter() {
-        rp = new Repository();
+        repo = new Repository();
     }
 
     public List<String> filterShoes(String brand, String model, String color, String size, String category) {
         List<String> list = new ArrayList<>();
 
-        rp.getCategoryMaps().stream()
+        repo.getCategoryMaps().stream()
                 .filter(a -> brand.isEmpty() || a.getShoe().getBrand().getName().contains(brand))
                 .filter(a -> model.isEmpty() || a.getShoe().getModel().contains(model))
                 .filter(a -> color.isEmpty() || a.getShoe().getColor().getName().contains(color))
                 .filter(a -> size.isEmpty() || a.getShoe().getSize().getEu() == Integer.parseInt(size))
                 .filter(a -> category.isEmpty() || a.getCategory().getName().contains(category))
                 .map(CategoryMap::getShoe)
-                .collect(Collectors.toMap(Shoe::getId, a -> a, (existingShoe, newShoe) -> existingShoe))
-                .values()
+                .collect(Collectors.toSet())
                 .forEach(a -> list.add(a.printShoe()));
 
         return list;
@@ -33,7 +32,7 @@ public class Reporter {
         int size = sizeStr.isEmpty()? 0: Integer.parseInt(sizeStr);
         List<String> list = new ArrayList<>();
 
-        rp.getShoppingCart().stream()
+        repo.getShoppingCart().stream()
                 .filter(a -> sizeStr.isEmpty()|| a.getShoe().getSize().getEu() == size)
                 .filter(a -> color.isEmpty() || a.getShoe().getColor().getName().equals(color))
                 .filter(a -> brand.isEmpty() || a.getShoe().getBrand().getName().equals(brand))
@@ -45,7 +44,7 @@ public class Reporter {
 
     public List<String> ordersPerCustomer() {
         List<String> list = new ArrayList<>();
-        rp.getOrders()
+        repo.getOrders()
                .stream()
                .collect(Collectors.groupingBy(OrderEntry::getCustomer))
                .forEach((k, v) -> list.add(k.getFirst_name() + " " + k.getLast_name() + ", " + v.size()));
@@ -54,7 +53,7 @@ public class Reporter {
 
     public List<String> moneySpentByCustomer() {
         List<String> list = new ArrayList<>();
-        rp.getShoppingCart().stream()
+        repo.getShoppingCart().stream()
                 .collect(Collectors.groupingBy(a -> a.getOrderEntry().getCustomer()))
                 .forEach((k, v) -> list.add(k.getFirst_name() + " " + k.getLast_name() + ", " + v.stream()
                                         .map(a -> a.getShoe().getPrice() * a.getQuantity())
@@ -65,7 +64,7 @@ public class Reporter {
 
     public List<String> moneySpentPerCity() {
         List<String> list = new ArrayList<>();
-        rp.getShoppingCart().stream()
+        repo.getShoppingCart().stream()
                 .collect(Collectors.groupingBy(a -> a.getOrderEntry().getCustomer().getCity()))
                 .forEach((k, v) -> list.add(k + ", " + v.stream()
                         .map(a -> a.getShoe().getPrice() * a.getQuantity())
@@ -77,7 +76,7 @@ public class Reporter {
     public List<String> mostSoldProducts() {
         List<String> list = new ArrayList<>();
         Map<String, Integer> map = new TreeMap<>();
-        rp.getShoppingCart().stream()
+        repo.getShoppingCart().stream()
                 .collect(Collectors.groupingBy(a -> a.getShoe().getBrand().getName() + ", " + a.getShoe().getModel()))
                 .forEach((k, v) -> map.put(k, v.stream().map(ShoppingCart::getQuantity).mapToInt(a -> a).sorted().sum()));
 
@@ -92,7 +91,7 @@ public class Reporter {
 
 
 
-    public Repository getRp() {
-        return rp;
+    public Repository getRepo() {
+        return repo;
     }
 }
